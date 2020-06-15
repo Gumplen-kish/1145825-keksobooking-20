fc2968b58195afe5d3f2b0840568783fcbfb8f53
Gumplen-kish <gumplen117@gmail.com>
2020-06-15 20:34:24 +0700

    Создаю пины

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 'use strict';
-//задаем переменные
-var AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var ADSAMOUNT = 8;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var Rooms = {
+  MIN: 1,
+  MAX: 6
+};
+var Guests = {
+  MIN: 1,
+  MAX: 10
+};
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg','http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var TITLES = [
   'Хорошая квартира',
   'Хижина, как у лесника',
@@ -25,90 +31,113 @@ var DESCRIPTION = [
   'Телевизор на всю стену',
   'Соседи ругаются, а иногда просятся к вам в гости'
 ];
-//Блок определения координат
-// Задаем координаты Y рандом от 130 до 630
-var locationYGenerate = function getRandomIntInclusive(min, max) {
-  min = Math.ceil('130');
-  max = Math.floor('630');
-  //Максимум и минимум включаются
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+var MapCoordinates = {
+  MIN_X: 0,
+  MAX_X: 1200,
+  MIN_Y: 130,
+  MAX_Y: 630
 };
-var y = locationYGenerate();
-// Задаем координаты X рандом от 0 до 1200
-var locationXGenerate = function getRandomIntInclusive(min, max) {
-  min = Math.ceil('0');
-  max = Math.floor('1200');
-  //Максимум и минимум включаются
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-var x = locationXGenerate();
-
-//Генерируем Price
-var PriceGenerate = function getRandomIntInclusive(min, max) {
-  min = Math.ceil('10000');
-  max = Math.floor('50000');
-  //Максимум и минимум включаются
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-//Генерируем ROOMS
-var RoomsGenerate = function getRandomIntInclusive(min, max) {
-  min = Math.ceil('1');
-  max = Math.floor('10');
-  //Максимум и минимум включаются
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-//Генерируем кол-во гостей
-var  GuestsGenerate = function getRandomIntInclusive(min, max) {
-  min = Math.ceil('1');
-  max = Math.floor('100');
-  //Максимум и минимум включаются
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-//Получаем случайное число
-var getRandomNumber = function (max) {
-  return Math.floor(Math.random() * Math.floor(max));
-};
-//Получаем случайную строчку из массива
-var getRandomElementFromArray = function (array) {
-  return array[getRandomNumber(array.length - 1)];
-};
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+var PIN_OFFSET_X = 50;
+var PIN_OFFSET_Y = 70;
 //Удаляем класс у карты для её отображения.
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-
-({
-  author: {
-    avatar: 'img/avatars/user' + getRandomElementFromArray(AVATARS) + '.png'
-  },
-  offer: {
-    title: getRandomElementFromArray(TITLES),
-    adress: '600, 350',
-    price: PriceGenerate(),
-    type:  getRandomElementFromArray(TYPES),
-    rooms: RoomsGenerate(),
-    guests: GuestsGenerate(),
-    checkin:  getRandomElementFromArray(TIMES),
-    checkout: getRandomElementFromArray(TIMES),
-    features: getRandomElementFromArray(FEATURES),
-    description: getRandomElementFromArray(Description),
-    photos: getRandomElementFromArray(PHOTOS)
-  },
-  location: x + ', ' + y
-});
-return adverts;
-
-//Пробую добавить пин
-var createPinsFragment = function (items) {
-  // находим блок в который будем вставлять наши метки
-    var pinsBlock = document.querySelector('.map__pins');
-  // Находим шаблон метки которую потом будем вставлять
-    var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-    for (var i = 0; i < items.length; i++) {
-      var htmlItem = renderPin(pinTemplate, items[i]);
-      fragment.appendChild(htmlItem);
-    }
-    pinsBlock.appendChild(fragment);
+/**
+ * @param {number} arr - массив
+ * @length - длинна массива
+ */
+var getRandomElementFromArray = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
+
+//Блок определения координат
+/**
+ * Функция выбора рандомного числа в диапозоне, где максимум и минимум включены
+ * @param {number} min - минимальное число диапозона
+ * @param {number} max - максимальное число диапозона
+ * @return {number} - рандомное число
+ */
+var getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+/**
+ * @param {number} ADSAMOUNT - Количество аватаров и максимальное количество пинов
+ * @return {number} - Возвращает рандомное число
+ */
+var getListOfOffers = function () {
+  var offers = [];
+  for (var i = 0; i < ADSAMOUNT; i++) {
+    offers[i] = createOffer(i + 1);
+  }
+  return offers;
+};
+/**
+ *Генерация массива моков из объектов
+ * @param {number} offerNumber - количество элеметов массива
+ * @return {Array} - Готовый массив с данными для пина
+ */
+var createOffer = function (offerNumber) {
+  var locationX = getRandomIntInclusive(MapCoordinates.MIN_X, MapCoordinates.MAX_X);
+  var locationY = getRandomIntInclusive(MapCoordinates.MIN_Y, MapCoordinates.MAX_Y);
+  var offer = {
+    author: {
+      avatar: 'img/avatars/user0' + (offerNumber) + '.png'
+    },
+    offer: {
+      title: getRandomElementFromArray(TITLES),
+      address: locationX + ', ' + locationY,
+      price: 1000,
+      type: getRandomElementFromArray(TYPES),
+      rooms: getRandomIntInclusive(Rooms.MIN, Rooms.MAX),
+      guests: getRandomIntInclusive(Guests.MIN, Guests.MAX),
+      checkin: getRandomElementFromArray(TIMES),
+      checkout: getRandomElementFromArray(TIMES),
+      features: getRandomElementFromArray(FEATURES),
+      description: getRandomElementFromArray(DESCRIPTION),
+      photos: getRandomElementFromArray(PHOTOS)
+    },
+    location: {
+      x: locationX,
+      y: locationY
+    }
+  };
+  return offer;
+};
+/**
+ * задаем расположение пина(ов)
+ * @param {Array} offer - массив с данными
+ */
+var createPin = function (offer) {
+  var pinTemplate = document.querySelector('#pin')
+    .content.querySelector('.map__pin');
+  var pin = pinTemplate.cloneNode(true);
+  var pinX = offer.location.x + PIN_OFFSET_X;
+  var pinY = offer.location.y + PIN_OFFSET_Y;
+  //
+  pin.style = 'left: ' + pinX + 'px; top: ' + pinY + 'px;';
+  var pinAvatar = pin.querySelector('img');
+  pinAvatar.src = offer.author.avatar;
+  pinAvatar.alt = offer.offer.title;
+  return pin;
+};
+/**
+ * Добавляем пины в разметку
+ * @param {Array} offers - массивы с данными
+ */
+var renderPins = function (offers) {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < offers.length; j++) {
+    fragment.appendChild(createPin(offers[j]));
+  }
+  var pins = document.querySelector('.map__pins');
+  pins.appendChild(fragment);
+};
+var offers = getListOfOffers();
+renderPins(offers);
